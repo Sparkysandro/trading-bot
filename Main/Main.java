201b11c670472475cfe53761ac0df42cc33e2854
Sparkysandro <sdiener2003@gmail.com>
2025-09-01 16:37:23 +0200

    started with first classes;

--- Main/Main.java
+++ Main/Main.java
@@ -1,11 +1,20 @@
 import java.util.*;
+import java.util.concurrent.ExecutorService;
 
-class Main {
+public class Main {
 
     public static void main(String[] args) {
         ExecutorService executorService = new ExecutorService(4);
 
+        ArrayList<String> tokens = new ArrayList<>();
+        for(int i = 0; i < 4; i++) {
+            tokens.add(new Token("t" + i, "t" + i));
+        }
 
+        ExecutorService executorService = new ExecutorService(4);
+        for(int i = 0; i < 4; i++) {
+            executorService.submit(new TokenTask(tokens.get(i), "thread" + i));
+        }
 
 
     }
